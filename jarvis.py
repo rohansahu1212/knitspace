1e19cfee98956fce1d7e758cd5bed3858686ba11
rohan sahu <rohansahu1212@gmail.com>
2021-08-22 20:26:01 +0530

    jarvis ban gya

--- jarvis.py
+++ jarvis.py
@@ -1,7 +1,19 @@
 import pyttsx3
 import speech_recognition as sr
 import wikipedia
+import os
+import pyjokes
+import webbrowser
+import datetime
+import wolframalpha
+import requests
+import subprocess
+from bs4 import BeautifulSoup as soup
+
+
+
 engine = pyttsx3.init("sapi5")
+MASTER="human"
 
 voices = engine.getProperty("voices")
 print(voices[0].id)
@@ -10,8 +22,22 @@ engine.setProperty("voices", voices[1].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
 def wishMe():
-    speak("mahika here, how may i assit you")
+    hour = int(datetime.datetime.now().hour)
+    
+
+    if hour>=0 and hour <12:
+        speak("good morning" + MASTER)
+
+    elif hour>=12 and hour<18:
+        speak("good afternoon" + MASTER)
+
+    else:
+        speak("good Evening" + MASTER)
+
+    speak("i am your assistant. How may I help you?")
+
 
 def takeCommand():
     r= sr.Recognizer()
@@ -32,19 +58,18 @@ def takeCommand():
 
 
 if __name__ == '__main__':
-    speak("i am queen mihika build with love by rohan ")
+    speak("i am jarvis, build with love by maratha coder ")
+    print("ask me anything")
     wishMe()
     while(True):
         query1=takeCommand().lower()
-        print("say, hey mahika")
+        print("say, hey jarvis")
         
         
-        if "mahika" in query1:
-            speak(" bolo bhos di ke rohan")
+        if "jarvis" in query1:
+            speak(" yes sir")
             print("listening....")
             query=takeCommand().lower()
-            
-
             if 'wikipedia' in query:
                 speak("searching wikipedia")
                 query=query.replace("wikipeda","")
@@ -52,4 +77,54 @@ if __name__ == '__main__':
                 speak("according to wikipedia")
                 speak(results)
                 print(results)
-        
+            elif 'joke' in query:
+                speak(pyjokes.get_joke())
+
+
+            elif 'open google' in query:
+
+                speak("here you go on google\n")
+            
+                webbrowser.open("www.google.com")
+
+            elif 'open youtube' in query:
+                speak("Here you go to youtube\n")
+                webbrowser.open("www.youtube.com")
+
+            elif "log off" in query or "sign out" in query:
+                speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
+                subprocess.call(["shutdown", "/l"])
+            
+            elif "play music" in query:
+                music_dir = "C:\\Users\\91700\\Music\\music"
+                songs = os.listdir(music_dir)
+                os.startfile(os.path.join(music_dir,songs[0]))
+            
+            elif "weather" in query:
+                api_key="8ef61edcf1c576d65d836254e11ea420"
+                base_url="https://api.openweathermap.org/data/2.5/weather?"
+                speak("whats the city name")
+                city_name=takeCommand()
+                complete_url=base_url+"appid="+api_key+"&q="+city_name
+                response = requests.get(complete_url)
+                x=response.json()
+                if x["cod"]!="404":
+                    y=x["main"]
+                    current_temperature = y["temp"]
+                    current_humidiy = y["humidity"]
+                    z = x["weather"]
+                    weather_description = z[0]["description"]
+                    speak(" Temperature in kelvin unit is " +
+                        str(current_temperature) +
+                        "\n humidity in percentage is " +
+                        str(current_humidiy) +
+                        "\n description  " +
+                        str(weather_description))
+                    print(" Temperature in kelvin unit = " +
+                        str(current_temperature) +
+                        "\n humidity (in percentage) = " +
+                        str(current_humidiy) +
+                        "\n description = " +
+                        str(weather_description))
+            else:
+                speak("sorry, say that again")
